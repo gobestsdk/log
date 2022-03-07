728f78091534a533e9cc7e7fa1085acf4c007871
timeloveboy <734991033@qq.com>
2022-03-07 11:46:50 +0800

    feat(test):log test

--- formatlog_test.go
+++ formatlog_test.go
@@ -2,14 +2,37 @@ package log
 
 import "testing"
 
+//import "github.com/gobestsdk/log"
+
 func TestPrint(t *testing.T) {
+	field := Fields{
+		"test": "ok",
+		"test2": map[string]interface{}{
+			"aname": 1,
+			"bname": "b",
+		},
+	}
 	Print(Fields{
 		"test": "ok",
 	})
+	SetSerializationtype("table")
+	Info(field)
+	SetSerializationtype("yaml")
+	Info(field)
 }
 
-func TestInfo(t *testing.T) {
+func TestInfoTable(t *testing.T) {
 	SetSerializationtype("table")
+	Info(Fields{
+		"test": "ok",
+		"test2": map[string]interface{}{
+			"aname": 1,
+			"bname": "b",
+		},
+	})
+}
+func TestInfoYAML(t *testing.T) {
+	SetSerializationtype("yaml")
 	Info(Fields{
 		"test": "ok",
 	})
